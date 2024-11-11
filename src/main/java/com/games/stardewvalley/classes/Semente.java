17ea0ebdb0f8f419ada9d4698fec49cd6fba7a9d
LuanSilva9 <luanpdev@gmail.com>
2024-11-11 12:07:06 -0400

    Adicionando conteudo a classe Semente

--- src/main/java/com/games/stardewvalley/classes/Semente.java
+++ src/main/java/com/games/stardewvalley/classes/Semente.java
@@ -8,6 +8,47 @@ package com.games.stardewvalley.classes;
  *
  * @author Luan
  */
-public class Semente {
+public class Semente extends Item {
+    private int valor;
+    private int status;
+    private int statusColheita;
+
+    public Semente(int valor, int status, int statusColheita, String nome, int raridade, String tipo, String descricao) {
+        super(nome, raridade, tipo, descricao);
+        this.valor = valor;
+        this.status = status;
+        this.statusColheita = statusColheita;
+    }
+    
+    
+    // Metodo(s) da semente
+    public int devolverDiferenca() {
+        return statusColheita - status;
+    }
+    
+    // Getters e Setters
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getStatusColheita() {
+        return statusColheita;
+    }
+
+    public void setStatusColheita(int statusColheita) {
+        this.statusColheita = statusColheita;
+    }
     
 }
